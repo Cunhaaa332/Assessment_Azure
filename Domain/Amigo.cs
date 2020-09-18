bd1053c1413996cae304a5f560792be130b8d031
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-18 14:25:39 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Amigo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain {
+    public class Amigo {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Foto { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime Birth { get; set; }
+        public List<Amigo> Amigos { get; set; }
+        public int IdPais { get; set; }
+        public string IdEstado { get; set; }
+    }
+}
+
---
+++ Domain/Pais.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain {
+    public class Pais {
+        public int Id { get; set; }
+        public string Bandeira { get; set; }
+        public string Nome { get; set; }
+        public List<Estado> Estados { get; set; }
+    }
+}
