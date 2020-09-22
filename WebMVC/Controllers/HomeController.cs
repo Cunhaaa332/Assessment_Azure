696b1d5aa4ac2b0233aba01a2efbdf82b521a329
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-22 17:57:30 -0300

    Contadores fuinando - Final Version.

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using WebMVC.Models;
+using WebMVC.Models.HomeModel;
 
 namespace WebMVC.Controllers {
     public class HomeController : Controller {
@@ -16,7 +19,22 @@ namespace WebMVC.Controllers {
         }
 
         public IActionResult Index() {
-            return View();
+            var client = new RestClient();
+
+            ListarTudoViewModel listarTudoViewModel = new ListarTudoViewModel();
+
+            var requestAmigo = new RestRequest("https://localhost:5001/api/amigos", DataFormat.Json);
+            var responseAmigo = client.Get<List<Amigo>>(requestAmigo);
+            listarTudoViewModel.Amigos = responseAmigo.Data.Count;
+            var requestPais = new RestRequest("https://localhost:5001/api/paises", DataFormat.Json);
+            var responsePais = client.Get<List<Pais>>(requestPais);
+            listarTudoViewModel.Paises = responsePais.Data.Count;
+            var requestEstado = new RestRequest("https://localhost:5001/api/estados", DataFormat.Json);
+            var responseEstado = client.Get<List<Estado>>(requestEstado);
+            listarTudoViewModel.Estados = responseEstado.Data.Count;
+
+
+            return View(listarTudoViewModel);
         }
 
         public IActionResult Privacy() {
---
+++ WebMVC/Models/HomeModel/ListarTudoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models.HomeModel {
+    public class ListarTudoViewModel {
+        public int Paises { get; set; }
+        public int Estados { get; set; }
+        public int Amigos { get; set; }
+    }
+}
